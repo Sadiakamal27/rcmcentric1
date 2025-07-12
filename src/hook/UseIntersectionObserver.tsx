cc729f9088d2c944d68507b65aec592326f9b343
sadiaKamal <sadiakamal2468@gmail.com>
2025-07-12 19:34:48 +0500

    observer for slideup animation

---
+++ src/hook/UseIntersectionObserver.tsx
@@ -0,0 +1,35 @@
+
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+
+export function useIntersectionObserver({ threshold = 0, rootMargin = "0px" }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          if (ref.current) {
+            observer.unobserve(ref.current); // Stop observing after animation triggers
+          }
+        }
+      },
+      { threshold, rootMargin }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, threshold, rootMargin]);
+
+  return [ref, isVisible];
+}
