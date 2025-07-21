8bc1bc9c7191f93753e0c118c1d02655a2de40d4
sadiaKamal <sadiakamal2468@gmail.com>
2025-07-21 20:35:55 +0500

    added Link for FAQs page

--- src/components/resources-dropdown.tsx
+++ src/components/resources-dropdown.tsx
@@ -17,9 +17,11 @@ export function ResourcesDropdown() {
         <DropdownMenuItem className="cursor-pointer hover:bg-gray-50 p-3 text-gray-700 hover:text-blue-600">
           Blog & Articles
         </DropdownMenuItem>
+         <Link href="/FAQs" target="_blank" rel="noopener noreferrer">
         <DropdownMenuItem className="cursor-pointer hover:bg-gray-50 p-3 text-gray-700 hover:text-blue-600">
           Read FAQ's
         </DropdownMenuItem>
+        </Link>
         <DropdownMenuItem className="cursor-pointer hover:bg-gray-50 p-3 text-gray-700 hover:text-blue-600">
           View Pricing
         </DropdownMenuItem>
